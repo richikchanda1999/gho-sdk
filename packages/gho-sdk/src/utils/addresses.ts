bb7f09b1ca4b79cb8d2a6bc011d0fdeecd8e22c2
Richik Chanda <richikchanda1999@gmail.com>
2024-01-21 17:55:18 +0530

    feat: included some AAVE addresses

--- packages/gho-sdk/src/utils/addresses.ts
+++ packages/gho-sdk/src/utils/addresses.ts
@@ -1,5 +1,6 @@
 const addresses: Record<number, Record<string, `0x${string}`>> = {
     1: {
+        // GHO Contracts
         GhoOracle: "0xD110cac5d8682A3b045D5524a9903E031d70FCCd",
         GhoATokenImpl: "0x2f32A274e02FA356423CE5e97a8e3155c1Ac396b",
         GhoVariableDebtTokenImpl: "0x3FEaB6F8510C73E05b8C0Fdf96Df012E3A144319",
@@ -11,8 +12,17 @@ const addresses: Record<number, Record<string, `0x${string}`>> = {
         Payload: "0x16765d275c00Caa7Ec9a30D1629fD42121c3ae6B",
         Create2Helper: "0xeCEBeDBF26013Fb55A5b0a275191A90984E5Ae5e",
         UiGhoDataProvider: "0x379c1EDD1A41218bdbFf960a9d5AD2818Bf61aE8",
+        // AAVE Contracts
+        ACLManager: "0xc2aaCf6553D20d1e9d78E365AAba8032af9c85b0",
+        Pool: "0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2",
+        PoolConfigurator: "0x64b761D848206f447Fe2dd461b0c635Ec39EbB27",
+        Incentives: "0x8164Cc65827dcFe994AB23944CBC90e0aa80bFcb",
+        PoolAddressProvider: "0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e",
+        WalletBalanceProvider: "0xC7be5307ba715ce89b152f3Df0658295b3dbA8E2",
+        AaveOracle: "0x54586bE62E3c3580375aE3723C145253060Ca0C2"
     },
     11155111: {
+        // GHO Contracts
         GhoToken: "0xc4bF5CbDaBE595361438F8c6a187bDc330539c60",
         GhoATokenImpl: "0xD4BDb51fB96996CA24a5C49E7b57f94a1850Fa30",
         GhoDiscountRateStrategy: "0x19cdecE64EDE475ba0EB114ff4E319d64Ef8ECCf",
@@ -28,6 +38,14 @@ const addresses: Record<number, Record<string, `0x${string}`>> = {
         GhoATokenProxy: "0xd190eF37dB51Bb955A680fF1A85763CC72d083D4",
         GhoStableDebtTokenProxy: "0xdCA691FB9609aB814E59c62d70783da1c056A9b6",
         GhoVariableDebtTokenProxy: "0x67ae46EF043F7A4508BD1d6B94DB6c33F0915844",
+        // AAVE Contracts
+        ACLManager: "0x7F2bE3b178deeFF716CD6Ff03Ef79A1dFf360ddD",
+        Pool: "0x0562453c3DAFBB5e625483af58f4E6D668c44e19",
+        PoolConfigurator: "0x63Dea961A0B2e2dF4F7A4bb61CBc39BF53A6eb32",
+        Incentives: "0xDe7562059fE64B3D088a26a3F8B60e77dCb81ebE",
+        PoolAddressProvider: "0x012bAC54348C0E635dCAc9D5FB99f06F24136C9A",
+        WalletBalanceProvider: "0xCD4e0d6D2b1252E2A709B8aE97DBA31164C5a709",
+        AaveOracle: "0x2da88497588bf89281816106C7259e31AF45a663"
     }
 }
 
